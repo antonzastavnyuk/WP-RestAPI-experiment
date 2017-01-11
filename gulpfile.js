9840edbcedc3f4b1f4b7b6a2e7d899b0129308b1
Anton Zastavnyuk <anton.zastavnyuk@gmail.com>
2017-01-11 19:01:54 +0200

    Return bower

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
-const gulp        = require('gulp');
-const sass        = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
 const concat = require('gulp-concat');
 const browserSync = require('browser-sync').create();
 
@@ -10,28 +10,43 @@ const Paths = {
     'src/scss/*.scss',
     'src/scss/**/*.scss'
   ],
+  fonts_source: [
+    'src/fonts/*'
+  ],
+  build_fonts: 'build/fonts/',
   sass_entry: 'src/scss/main.scss',
   build_js: 'build/js/',
   js_source: [
-    'src/js/entry.js'
+    'bower_components/jquery/dist/jquery.min.js',
+    'bower_components/masonry/dist/masonry.pkgd.min.js',
+    'bower_components/imagesloaded/imagesloaded.pkgd.min.js',
+    'bower_components/handlebars/handlebars.min.js',
+    'src/js/entry.js',
   ],
 };
 
-gulp.task('serve', ['style', 'scripts'], function() {
-    browserSync.init({
-        server: "./"
-    });
-    gulp.watch(Paths.sass_source, ['style']);
-    gulp.watch(Paths.js_source, ['scripts']);
-    gulp.watch("*.html").on('change', browserSync.reload);
+gulp.task('serve', ['style', 'scripts', 'fonts'], function () {
+  browserSync.init({
+    server: './'
+  });
+  gulp.watch(Paths.sass_source, ['style']);
+  gulp.watch(Paths.js_source, ['scripts']);
+  gulp.watch(Paths.fonts_source, ['fonts']);
+  gulp.watch('*.html').on('change', browserSync.reload);
 });
 
-gulp.task('style', function() {
-    return gulp.src(Paths.sass_entry)
+gulp.task('style', function () {
+  return gulp.src(Paths.sass_entry)
         .pipe(sass())
         .pipe(concat('main.css'))
         .pipe(gulp.dest(Paths.build_css))
         .pipe(browserSync.stream());
+});
+
+gulp.task('fonts', function () {
+  return gulp.src(Paths.fonts_source)
+    .pipe(gulp.dest(Paths.build_fonts))
+    .pipe(browserSync.stream());
 });
 
 gulp.task('scripts', function (production) {
