9e7fe2dd60d551fa42b3fad3f2f824ba03ed13b7
Anton Zastavnyuk <anton.zastavnyuk@gmail.com>
2017-01-25 19:07:38 +0200

    Improve render function

--- src/js/entry.js
+++ src/js/entry.js
@@ -1,8 +1,6 @@
 var publicationsUrl = 'http://macmillan.zindex.host/wp-json/wp/v2/publications/';
 var postsUrl = 'http://macmillan.zindex.host/wp-json/wp/v2/posts/';
 var presentationsUrl = 'http://macmillan.zindex.host/wp-json/wp/v2/presentations/';
-$('.wrapper').append('<ul />');
-$('ul').addClass('social-media__list');
 
 function getData(url) {
   return new Promise(function (resolve, reject) {
@@ -23,31 +21,57 @@ function getData(url) {
   });
 }
 
-
-function render(value) {
-  var postTypeClass;
-  var date = new Date(value[0]);
+function getItem(item) {
+  var date = new Date(item.date);
   var year = date.getFullYear();
   var month = date.getMonth() + 1;
   var day = date.getDay();
-
-  if (value[5] === 'publications') {
+  var postTypeClass;
+  if (item.type === 'publications') {
     postTypeClass = ' social-media__item-content--publications';
-  } else if (value[5] === 'presentations') {
+  } else if (item.type === 'presentations') {
     postTypeClass = ' social-media__item-content--tweet';
-  } else if (value[5] === 'post') {
+  } else if (item.type === 'post') {
     postTypeClass = ' social-media__item-content--post';
   }
-  var renderStr = '<ul class="social-media__list"><li class="social-media__item" ><div class="social-media__item-content' + postTypeClass + '"><div class="social-media__item-description"><div class="social-media__item-image-container" style="height: 89px;"><img src="' + value[4] + '" style="height: 100%;"></div><div class="social-media__item-title-container"><p>' + value[1] + '</p><span class="social-media__item-author">' + value[2] + '</span></div></div><div class="social-media__item-meta"><a class="social-media__item-link" href="' + value[4] + '" target="_blank">view</a><span class="social-media__item-date">' + year + '.' + day + '.' + month + '</span></div></div></li></ul>';
-  $('.wrapper').append(renderStr);
+
+  return [
+    '<li class="social-media__item" >',
+      '<div class="social-media__item-content' + postTypeClass + '">',
+        '<div class="social-media__item-description">',
+          '<div class="social-media__item-image-container" style="height: 89px;">',
+            '<img src="' + item.image + '" style="height: 100%;">',
+          '</div>',
+          '<div class="social-media__item-title-container">',
+            '<p>' + item.title + '</p>',
+            '<span class="social-media__item-author">' + item.author + '</span>',
+          '</div>',
+        '</div>',
+      '<div class="social-media__item-meta">',
+        '<a class="social-media__item-link" href="' + item.pdf + '" target="_blank">view</a>',
+          '<span class="social-media__item-date">' + year + '.' + day + '.' + month + '</span>',
+        '</div>',
+      '</div>',
+    '</li>'
+  ].join('\n');
+}
+
+
+function render(value) {
+  var renderedItems = value.map(function (item) {
+    return getItem(item);
+  });
+  // console.log(renderedItems);
+  $('<ul/>')
+    .append(renderedItems)
+  .appendTo('.wrapper');
 }
 
 var publicationsResult = getData(publicationsUrl);
 var presentationsResult = getData(presentationsUrl);
 var postsResult = getData(postsUrl);
 
-Promise.all([publicationsResult, presentationsResult, postsResult])
-.then(function (allResults) {
+Promise.all([publicationsResult, presentationsResult, postsResult]).then(function (allResults) {
   var allResultsConcat = allResults.reduce(function (a, b) {
     return a.concat(b);
   });
@@ -56,10 +80,15 @@ Promise.all([publicationsResult, presentationsResult, postsResult])
     return (new Date(b.date)).getTime() - (new Date(a.date)).getTime();
   });
 
+  var readyResults = {};
+  readyResults.items = [];
+  var image = [];
+  var allImages = [];
+  var finalResult = [];
+
   allResultsConcat.forEach(function (item) {
     var author;
     var pdf;
-    var finalResult;
     var imageHref = item._links['wp:featuredmedia'][0].href;
 
     if (item.type === 'publications') {
@@ -73,22 +102,29 @@ Promise.all([publicationsResult, presentationsResult, postsResult])
       pdf = '';
     }
 
-    getData(imageHref)
-    .then(function (data) {
-      return data.source_url;
-    }, function (data) {
-      return data;
-    })
-    .then(function (result) {
-      finalResult = [
-        item.date,
-        item.title.rendered,
-        author,
-        pdf,
-        result,
-        item.type];
-
-      render(finalResult);
+    allImages.push(getData(imageHref));
+    finalResult.push({
+      date: item.date,
+      title: item.title.rendered,
+      author: author,
+      pdf: pdf,
+      type: item.type
     });
+  });
+
+  Promise.all(allImages)
+  .then(function (images) {
+    var resultsToRender = finalResult.map(function (item, index) {
+      return {
+        date: item.date,
+        title: item.title,
+        author: item.author,
+        pdf: item.pdf,
+        type: item.type,
+        image: images[index].source_url,
+      };
+    });
+
+    render(resultsToRender);
   });
 });
